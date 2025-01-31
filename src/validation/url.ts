ec05aa0f4b06f79852f22b4d2ca6d8586c5571e1
proyectowoody <proyectowoody@gmail.com>
2025-01-31 02:45:11 -0400

    Primer commit

--- src/validation/url.ts
+++ src/validation/url.ts
@@ -1,8 +1,8 @@
 
 // Rutas backend
-// export const api = 
-// export const apiFront = 
+export const api ="https://mediakit-backend.vercel.app";
+export const apiFront = "https://media-kit1.netlify.app"; 
 
 // Rutas frontend
-export const apiFront = "http://localhost:5173"
-export const api = "http://localhost:4001"
+// export const apiFront = "http://localhost:5173"
+// export const api = "http://localhost:4001"
